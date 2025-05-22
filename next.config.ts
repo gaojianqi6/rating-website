54c8107e337f015dbd5ff89bc1a7f2eb3ca11cdc
Jianqi Gao <jianqi.gao@carsome.com>
2025-05-22 16:43:23 +1200

    Use next to management the api proxy

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+			},
+		]
+	},
 };
 
 export default nextConfig;
