004af19b16adf3698ec1a5df136baf92fb39c806
Jianqi Gao <jianqi.gao@carsome.com>
2025-05-22 16:54:07 +1200

    Use difference next proxy api variable

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ const nextConfig: NextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+				destination: `${process.env.NEXT_PROXY_API_URL}/:path*`,
 			},
 		]
 	},
